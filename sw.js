28de24bb9260a47c0e06a06d34d13f44398cf716
ashvaren <mike@ashvaren.com>
2026-09-04 16:38:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ironfield-v2';
+const CACHE='ironfield-v4';
 const FILES=['./','./index.html','./manifest.json','./icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
